eac67558bffd6d335a525cda6b078f4bfd43aafd
hoanghuulong <hoanghuulong.krn@gamil.com>
2021-07-04 01:56:00 +0700

    update courses seeder

--- database/seeders/CoursesSeeder.php
+++ database/seeders/CoursesSeeder.php
@@ -18,9 +18,8 @@ class CoursesSeeder extends Seeder
         for ($i=0; $i < 20; $i++) { 
             DB::table('courses')->insert([
                 'course_name' => Str::random(10),
-                'course_semaster' => Str::random(10),
+                'course_semester' => Str::random(10),
                 'course_year' => mt_rand(2016, 2021),
-                'subject_id' => mt_rand(1, 20),
                 'status' => 1,
             ]);
         }
