0838cd44487cdf113275723d9a54b031a7cfffc8
hoanghuulong <hoanghuulong.krn@gamil.com>
2021-07-03 22:53:20 +0700

    update

--- views/layout.blade.php
+++ views/layout.blade.php
@@ -8,9 +8,9 @@
 
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
-    <link rel="stylesheet" href="  {!! asset('packages/cansa/package-intership/bootstrap/css/bootstrap.min.css') !!}">
-    <link rel="stylesheet" href="  {!! asset('packages/cansa/package-intership/fonts/fontawesome-all.min.css') !!}">
-    <link rel="stylesheet" href="  {!! asset('packages/cansa/package-intership/fonts/font-awesome.min.css') !!}">
+    <link rel="stylesheet" href="  {!! asset('packages/courses/package-courses/bootstrap/css/bootstrap.min.css') !!}">
+    <link rel="stylesheet" href="  {!! asset('packages/courses/package-courses/fonts/fontawesome-all.min.css') !!}">
+    <link rel="stylesheet" href="  {!! asset('packages/courses/package-courses/fonts/font-awesome.min.css') !!}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="stylesheet" href="  {!! asset('packages/cansa/package-intership/fonts/fontawesome5-overrides.min.css') !!}">
 </head>
@@ -18,9 +18,9 @@
     @yield('content')
 
     <!-- Footer -->
-    {!! Html::script('packages/cansa/package-courses/js/jquery-3.6.0.min.js') !!}
-    {!! Html::script('packages/cansa/package-courses/bootstrap/js/bootstrap.min.js') !!}
-    {!! Html::script('packages/cansa/package-courses/js/bs-init.js') !!}
-    {!! Html::script('packages/cansa/package-courses/js/theme.js') !!}
-    <script id="bs-live-reload" data-sseport="50639" data-lastchange="1625067528989" src="{!! asset('packages/cansa/package-intership//js/livereload.js') !!}"></script>
+    {!! Html::script('packages/courses/package-courses/js/jquery-3.6.0.min.js') !!}
+    {!! Html::script('packages/courses/package-courses/bootstrap/js/bootstrap.min.js') !!}
+    {!! Html::script('packages/courses/package-courses/js/bs-init.js') !!}
+    {!! Html::script('packages/courses/package-courses/js/theme.js') !!}
+    <script id="bs-live-reload" data-sseport="50639" data-lastchange="1625067528989" src="{!! asset('packages/courses/package-courses//js/livereload.js') !!}"></script>
 </html>
